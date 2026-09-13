ed9bf4b55c6e0c2d18ce96b7fec69bd5fbe8533d
Max-O-67 <m.oli.paquet@gmail.com>
2026-09-13 19:11:28 -0400

    Ajout des assemblages auto et manuel en plus de la reponse dans le fichier MD

---
+++ DECISIONS.md
@@ -0,0 +1,3 @@
+J'ai choisi le cycle de vie Scoped.
+Je l'ai choisi pour que les services soient créés une fois par scope et réutilisés pendant ce scope. Ça permet aussi de respecter le cycle de vie demandé pour l'exercice.
+
--- semaine-03/S03E04_Restaurant_Composition/S03E04_Restaurant_Composition.Terminal/Program.cs
+++ semaine-03/S03E04_Restaurant_Composition/S03E04_Restaurant_Composition.Terminal/Program.cs
@@ -1,1 +1,41 @@
-Console.Out.WriteLine("Complétez les assemblages manuel et automatisé.");
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurant.Application;
+using Restaurant.Infrastructure;
+
+if (args.Contains("--manuel"))
+{
+    AssemblageManuel();
+}
+else
+{
+    AssemblageAutomatique(args);
+}
+
+static void AssemblageManuel()
+{
+    IDepotCommandes depot = new DepotCommandesMemoire();
+    INotificationCommande notification = new NotificationConsole();
+
+    CreerCommande creerCommande = new CreerCommande(depot, notification);
+
+    creerCommande.Executer(123);
+}
+
+static void AssemblageAutomatique(string[] args)
+{
+    var builder = Host.CreateApplicationBuilder(args);
+
+    builder.Services.AddScoped<IDepotCommandes, DepotCommandesMemoire>();
+    builder.Services.AddScoped<INotificationCommande, NotificationConsole>();
+    builder.Services.AddScoped<CreerCommande>();
+
+    var app = builder.Build();
+
+    using var scope = app.Services.CreateScope();
+
+    CreerCommande creerCommande =
+        scope.ServiceProvider.GetRequiredService<CreerCommande>();
+
+    creerCommande.Executer(123);
+}
